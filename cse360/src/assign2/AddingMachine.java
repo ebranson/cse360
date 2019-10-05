672a12a133f3d833b912ff58a0696fe20979cf9e
Eric Branson <ebranson@asu.edu>
2019-10-05 12:03:14 -0700

    Added the addingmachine class and commented using javadoc

---
+++ cse360/src/assign2/AddingMachine.java
@@ -0,0 +1,52 @@
+package assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * returns the total number being tracked by the AddingMachine
+	 * @return
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * takes an input value and adds it to the total
+	 * @param value
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * takes an input value and subtracts it from the total
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 * tracks the total operation history of the AddingMachine
+	 */
+	
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * clears all current operations of the AddingMachine
+	 */
+	
+	public void clear() {
+	
+	}
+}
