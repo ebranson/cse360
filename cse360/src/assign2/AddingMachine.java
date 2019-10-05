0503baf4a74207821298b8eee399828ec0b52c40
Eric Branson <ebranson@asu.edu>
2019-10-05 12:55:07 -0700

    All methods implemented for AddingMachine

--- cse360/src/assign2/AddingMachine.java
+++ cse360/src/assign2/AddingMachine.java
@@ -3,6 +3,7 @@ package assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history = "0";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
@@ -14,24 +15,26 @@ public class AddingMachine {
 	 */
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
-	 * takes an input value and adds it to the total
+	 * takes an input value and adds it to the total then updates the history
 	 * @param value
 	 */
 	
 	public void add (int value) {
-		
+		total = total + value;
+		history = history.concat("\n+" + value);
 	}
 	
 	/**
-	 * takes an input value and subtracts it from the total
+	 * takes an input value and subtracts it from the total then updates the history
 	 * @param value
 	 */
 	public void subtract (int value) {
-		
+		total = total - value;
+		history = history.concat("\n-" + value);
 	}
 		
 	/**
@@ -39,7 +42,7 @@ public class AddingMachine {
 	 */
 	
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	/**
@@ -47,6 +50,20 @@ public class AddingMachine {
 	 */
 	
 	public void clear() {
-	
+		total = 0;
+		history = "0";	
 	}
+
+/*
+//used to test the implementation
+
+public static void main(String args[]) {
+	AddingMachine a1 = new AddingMachine();
+	
+	a1.add(5);
+	a1.subtract(3);
+	System.out.println(a1.toString());
+	a1.clear();
+	System.out.println(a1.toString());
+}*/
 }
